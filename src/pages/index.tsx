25cc810afdb80a196c91f9e24e6befe481fe9dc7
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-06-11 23:03:46 +0700

    feat: make the cell square

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,17 +5,17 @@ export default function HomePage() {
     <main>
       <div className='layout flex min-h-screen flex-col justify-center text-center'>
         {[0, 1, 2].map((row) => (
-          <div key={row} className='my-3'>
+          <div key={row} className='flex flex-row justify-center'>
             {[0, 1, 2].map((col) => (
               <button
                 key={row + '|' + col}
-                className='mx-3'
+                className='h-20 w-20 items-center justify-center border-2 border-solid text-center'
                 id={row + '|' + col}
                 onClick={(e) => {
                   alert(e.currentTarget.id);
                 }}
               >
-                Test {row} {col}
+                <h3 className='text-xl'>XO</h3>
               </button>
             ))}
           </div>
