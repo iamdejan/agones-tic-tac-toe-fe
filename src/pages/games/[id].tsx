dd3066e06f68de8576e376acd8773a26daff1473
Giovanni Dejan <giovanni.dejan@binus.ac.id>
2022-06-12 15:54:29 +0700

    feat: create tic-tac-toe layout (#1)

---
+++ src/pages/games/[id].tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export default function HomePage() {
+  return (
+    <main>
+      <div className='layout flex min-h-screen flex-col justify-center text-center'>
+        {[0, 1, 2].map((row) => (
+          <div key={row} className='flex flex-row justify-center'>
+            {[0, 1, 2].map((col) => (
+              <button
+                key={row + '|' + col}
+                className='h-20 w-20 items-center justify-center border-2 border-solid text-center'
+                id={row + '|' + col}
+                onClick={(e) => {
+                  alert(e.currentTarget.id);
+                }}
+              >
+                <h3 className='text-xl'>XO</h3>
+              </button>
+            ))}
+          </div>
+        ))}
+      </div>
+    </main>
+  );
+}
