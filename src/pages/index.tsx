f8d4c0fe23d23488304116636caac24dab5f272e
Giovanni Dejan <giovanni.dejan@traveloka.com>
2022-06-19 22:25:10 +0700

    fix: runtime error caused by mongoose and webpack

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,50 +1,30 @@
-import { nanoid } from 'nanoid';
 import * as React from 'react';
 
-import GameSchema from '@/models/GameSchema';
-
-import Game from '@/types/Game';
-import MongoUtility from '@/utilities/MongoUtility';
-
-function createGameServer(): string {
-  // TODO: provision Agones in prod or localhost, depends on env var
-  return 'localhost:3001';
-}
-
-function createGame(): string {
-  const serverUrl = createGameServer();
-  const gameId = nanoid();
-
-  const game: Game = { id: gameId, players: [], serverUrl };
-  GameSchema.build(game).save();
-
-  return gameId;
-}
-
-function joinGame(gameId: string) {
-  GameSchema.findOne({
-    id: gameId,
-  }).then((game) => {
-    if (!game) {
-      return;
-    }
-
-    // TODO dejan: connect socket-io to game server
-    const socketId = '';
-    // TODO dejan: join
-    GameSchema.updateOne({ gameId }, { players: [...game.players, socketId] });
-  });
-}
-
-function createAndJoinGame() {
-  const gameId = createGame();
-  joinGame(gameId);
-}
-
 export default function HomePage() {
-  React.useEffect(() => {
-    MongoUtility.connectToDatabase();
-  }, []);
+  function createAndJoinGame() {
+    const gameId = createGame();
+    joinGame(gameId);
+  }
+
+  function createGame(): string {
+    const serverUrl = createGameServer();
+
+    // TODO dejan: call agones-tic-tac-toe-be-db
+    const gameId = '';
+
+    return gameId;
+  }
+
+  function createGameServer(): string {
+    // TODO: provision Agones in prod or localhost, depends on env var
+    return 'localhost:3001';
+  }
+
+  function joinGame(gameId: string) {
+    // TODO dejan:
+    //  1) get server url
+    //  2) connect to server url
+  }
 
   return (
     <main>
--- src/types/Game.ts
+++
@@ -1,7 +0,0 @@
-type Game = {
-  id: string;
-  players: string[];
-  serverUrl: string;
-};
-
-export default Game;
